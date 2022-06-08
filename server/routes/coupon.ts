5bc2d55f6a0a43bbbdbc5ead39e97b78eae20c5f
Sérgio Righi <sergiohautrive@gmail.com>
2022-06-08 12:47:35 -0400

    --optimize_for_size added

--- server/routes/coupon.ts
+++ server/routes/coupon.ts
@@ -5,10 +5,8 @@ import { CouponsController } from "@server/controllers";
 const router = express.Router();
 
 router.get("/", (_, res) => CouponsController.all(res));
-router.get("/:id", validation, (req, res) => CouponsController.find(res, req));
 router.post("/", (req, res) => CouponsController.create(res, req));
 router.post("/one", (req, res) => CouponsController.findOne(res, req));
-router.post("/exists", (req, res) => CouponsController.exists(res, req));
 router.put("/", (req, res) => CouponsController.update(res, req));
 router.patch("/soft/:id", validation, (req, res) => CouponsController.soft(res, req));
 router.patch("/restore/:id", validation, (req, res) => CouponsController.restore(res, req));
--- server/routes/order.ts
+++ server/routes/order.ts
@@ -4,13 +4,9 @@ import { OrdersController } from "@server/controllers";
 
 const router = express.Router();
 
-router.get("/", (_, res) => OrdersController.all(res));
 router.get("/:id", validation, (req, res) => OrdersController.find(res, req));
 router.post("/", (req, res) => OrdersController.create(res, req));
-router.post("/one", (req, res) => OrdersController.findOne(res, req));
-router.post("/many", (req, res) => OrdersController.findMany(res, req));
 router.post("/all-with-books", (req, res) => OrdersController.allWithBooks(res, req));
-router.post("/exists", (req, res) => OrdersController.exists(res, req));
 router.put("/", (req, res) => OrdersController.update(res, req));
 router.patch("/soft/:id", validation, (req, res) => OrdersController.soft(res, req));
 router.patch("/restore/:id", validation, (req, res) => OrdersController.restore(res, req));
